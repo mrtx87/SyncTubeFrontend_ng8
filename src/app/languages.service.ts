079c062c6cd20e8c9ffba73d23b8f60366e235a3
Patrick Beiersdörfer <patrick.beiersdoerfer@arxes-tolina.de>
2019-08-19 22:29:57 +0200

    mehrere sprachstubs hinzugefügt inklusive icons und objekte

--- src/app/languages.service.ts
+++ src/app/languages.service.ts
@@ -13,15 +13,26 @@ export class LanguagesService {
   /***
    * 
    *  Um eine neue Sprache zu registrieren legst du eine Sprachdatei an (landname.json) -> beispiel: assets/languages/german.json. 
-   *  Am besten kopierst du eine vorhandene Sprachdatei als Grundlage und tauschst nur die texte aus und passt den Dateinamen an. 
+   *  Am besten kopierst du die blank_.json Sprachdatei als Grundlage und tauschst nur die texte aus und passt den Dateinamen an. 
    *  Der Name der Datei ist der key der hier als key hinzugefügt werden muss. Es muss noch ein passendes icon in assets/languages/icons/lang.svg hinterlegt werden.
    *  Damit legst du eine neue Sprache an unter LANGUAGE_LIST. Bsp. new Language('german', 'assets/languages/icons/germany.svg')
    *  Das ist alles was du tun musst um eine neue Sprache hinzuzufügen. 
    * 
+   * 
+   *  !!! ACHTUNG WER NEUE VARIABLEN ERGÄNZT MUSS DIESE IN DER blank_.json und ALLEN SPRACHDATEIEN HINZUFÜGEN 
    */
 
 
-  private LANGUAGE_LIST: Language[] = [new Language('german', 'assets/languages/icons/germany.svg'), new Language('english', 'assets/languages/icons/united-kingdom.svg')];
+  private LANGUAGE_LIST: Language[] = [new Language('german', 'assets/languages/icons/germany.svg'),
+   new Language('english', 'assets/languages/icons/united-kingdom.svg'),
+   new Language('turkey', 'assets/languages/icons/turkey.svg'),
+   new Language('russia', 'assets/languages/icons/russia.svg'),
+   new Language('france', 'assets/languages/icons/france.svg'),
+   new Language('usa', 'assets/languages/icons/united-states-of-america.svg'),
+   new Language('hebrew', 'assets/languages/icons/israel.svg')
+
+
+  ];
 
 
   private LANGUAGES: Map<string, any> = new Map<string, any>();
